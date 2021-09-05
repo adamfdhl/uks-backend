ae621c008ae329e2949e8bbfe5cb88f2a10a1b72
adamfdhl <adamfdhl@gmail.com>
2021-09-06 06:56:48 +0700

    added total 4 skkni

---
+++ utils/constants.py
@@ -0,0 +1,438 @@
+map_id_unit_to_skkni = {
+  1 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  2 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  3 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  4 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  5 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  6 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  7 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  8 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  9 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  10 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  11 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  12 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  13 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  14 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  15 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  16 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  17 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  18 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  19 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  20 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  21 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  22 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  23 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  24 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  25 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  26 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  27 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  28 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  29 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  30 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  31 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  32 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  33 : {
+    "skkni": "SKKNI Nomor 610 Tahun 2012 : Manajemen Layanan Teknologi Informasi"
+  },
+  34 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  35 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  36 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  37 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  38 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  39 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  40 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  41 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  42 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  43 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  44 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  45 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  46 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  47 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  48 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  49 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  50 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  51 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  52 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  53 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  54 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  55 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  56 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  57 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  58 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  59 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  60 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  61 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  62 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  63 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  64 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  65 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  66 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  67 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  68 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  69 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  70 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  71 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  72 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  73 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  74 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  75 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  76 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  77 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  78 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  79 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  80 : {
+    "skkni": "SKKNI Nomor 282 Tahun 2016 : Software Development - Pemrograman"
+  },
+  81 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  82 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  83 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  84 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  85 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  86 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  87 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  88 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  89 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  90 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  91 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  92 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  93 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  94 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  95 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  96 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  97 : {
+    "skkni": "SKKNI Nomor 45 Tahun 2015 : Pengelolaan Pusat Data"
+  },
+  98 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  99 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  100 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  101 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  102 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  103 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  104 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  105 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  106 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  107 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  108 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  109 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  110 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  111 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  112 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  113 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  114 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  115 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  116 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  117 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  118 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  119 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  120 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  121 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  122 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  123 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  124 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  125 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  126 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  127 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  128 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  129 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  130 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  131 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  132 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  133 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  134 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  135 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  136 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  137 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  138 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  139 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  140 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  141 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  142 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  143 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  144 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  145 : {
+    "skkni": "SKKNI Nomor 55 Tahun 2015 : Keamanan Informasi"
+  },
+  
+}
